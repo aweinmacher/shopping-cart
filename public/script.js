451e9d407bd6129ae574cdeb7d1a9eae34c23362
Anna W <aweinmacher@gmail.com>
2018-01-02 17:41:03 +0200

    total price is shown

--- public/script.js
+++ public/script.js
@@ -2,14 +2,19 @@ var cart = [];
 
 var updateCart = function () {
   $(".cart-list").empty();
-  var cartData = {"cart": cart};
+  var cartData = { "cart": cart };
   console.log(cartData);
   var source = $("#cart-template").html();
   var template = Handlebars.compile(source);
   var newHTML = template(cartData);
   $(".cart-list").append(newHTML);
-  // TODO: Write this function. In this function we render the page.
-  // Meaning we make sure that all our cart items are displayed in the browser.
+  
+  $(".total").empty();
+  var totalPrice = 0;
+  for (var item in cart) {
+    totalPrice += cart[item].price;
+  }
+  $(".total").append(totalPrice);
 }
 
 
@@ -24,7 +29,7 @@ var clearCart = function () {
 }
 
 $('.view-cart').on('click', function () {
-  $(".shopping-cart").toggleClass('show'); 
+  $(".shopping-cart").toggleClass('show');
 });
 
 $('.add-to-cart').on('click', function () {
