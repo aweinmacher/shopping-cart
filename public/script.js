c2c7a480715c0ca4932b1e04e6b4ff5deb6fe682
Anna W <aweinmacher@gmail.com>
2018-01-02 17:31:11 +0200

    adding to shopping cart

--- public/script.js
+++ public/script.js
@@ -1,20 +1,26 @@
-// an array with all of our cart items
 var cart = [];
 
 var updateCart = function () {
+  $(".cart-list").empty();
+  var cartData = {"cart": cart};
+  console.log(cartData);
+  var source = $("#cart-template").html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(cartData);
+  $(".cart-list").append(newHTML);
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
 }
 
 
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+  cart.push(item);
+  console.log(cart);
 }
 
 var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
+  cart = [];
+  updateCart();
 }
 
 $('.view-cart').on('click', function () {
@@ -22,7 +28,10 @@ $('.view-cart').on('click', function () {
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
+  var item = {
+    name: $(this).closest(".item").data().name,
+    price: $(this).closest(".item").data().price
+  }
   addItem(item);
   updateCart();
 });
