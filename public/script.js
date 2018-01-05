4b3efc8cafbbc645c9b90e2ff1f667bb4fd8f6b5
Anna W <aweinmacher@gmail.com>
2018-01-05 18:34:02 +0200

    quantity and total cost

--- public/script.js
+++ public/script.js
@@ -8,18 +8,28 @@ var updateCart = function () {
   var template = Handlebars.compile(source);
   var newHTML = template(cartData);
   $(".cart-list").append(newHTML);
-  
+
   $(".total").empty();
   var totalPrice = 0;
   for (var item in cart) {
-    totalPrice += cart[item].price;
+    totalPrice += cart[item].price * cart[item].quantity;
   }
   $(".total").append(totalPrice);
 }
 
-
-var addToCart = function (item) {
-  cart.push(item);
+var addToCart = function (name, price) {
+  for (var item in cart) {
+    if (cart[item].name === name && cart[item].price === price) {
+      cart[item].quantity++;
+      console.log(cart);
+      return;
+    }
+  }
+  cart.push({
+    name: name,
+    price: price,
+    quantity: 1
+  });
   console.log(cart);
 }
 
@@ -33,11 +43,9 @@ $('.view-cart').on('click', function () {
 });
 
 $('.container').on('click', '.add-to-cart', function () {
-  var item = {
-    name: $(this).closest(".item").data().name,
-    price: $(this).closest(".item").data().price
-  }
-  addToCart(item);
+  var name = $(this).closest(".item").data().name;
+  var price = $(this).closest(".item").data().price;
+  addToCart(name, price);
   updateCart();
 });
 
